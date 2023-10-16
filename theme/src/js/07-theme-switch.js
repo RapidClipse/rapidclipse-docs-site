672e2e93e51542a2c9eddc23f4342410557be586
Elias <e.keilhammer@xdev-software.de>
2023-10-16 10:24:33 +0200

    darkmode fix

--- theme/src/js/07-theme-switch.js
+++ theme/src/js/07-theme-switch.js
@@ -5,8 +5,6 @@ function toDarkTheme (resolve) {
   localStorage.setItem('theme', 'dark')
   document.querySelector('html').setAttribute('data-theme', 'dark')
 
-  // Update highlight js theme
-  //enableHightLightDarkTheme(true)
   // Promise.resolve if existing
   resolve && resolve()
 }
@@ -15,25 +13,8 @@ function toLightTheme (resolve) {
   localStorage.setItem('theme', 'light')
   document.querySelector('html').setAttribute('data-theme', 'light')
 
-  // Update highlight js theme
-  //enableHightLightDarkTheme(false)
   // Promise.resolve if existing
   resolve && resolve()
-}
-
-// Update highlight js to dark theme if dark = true
-function enableHightLightDarkTheme (dark) {
-  var hljsCssLink = document.getElementById('highlight-style-lnk')
-  if (hljsCssLink) {
-    var currentHref = hljsCssLink.getAttribute('href')
-    var cssHref = currentHref.replace('-dark', '-light')
-    if (dark) {
-      cssHref = currentHref.replace('-light', '-dark')
-    }
-    hljsCssLink.setAttribute('href', cssHref)
-  } else {
-    console.log('Failed to find highlight-style-lnk css link element in page, can not swap theme')
-  }
 }
 
 function performThemeSwitch (checkbox, switchBall) {
